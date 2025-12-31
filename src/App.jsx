2696264b9f479de53edb75c41973eb2069740ca2
MiyaZhao27 <miya.zhao@yale.edu>
2025-12-31 16:04:27 -0500

    spelling

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,8 @@ export default function App() {
             I'm a third-year pursuing my Bachelors of Science in Statistics and Data Science with a Certificate in Programming at Yale University.
           </p>
           <p className="ep-sub">
-            My diverse experience in research development, the visual arts, education, and event planning has been instrumental in shaping my skill set and passion for bringing things from  <strong> ideation to implementation</strong>. The core principles that guide me and the roles I take on include <strong>creativity, a people-first mindset, and sustainability</strong>. This commitment to turning vision into reality <strong>with intentionality</strong> is what drives my interest in Product Management, particularly within (Bio)Tech Development, Strategy, and Data Analytics. I want to do work that solves problems with high effort and attention to detail no matter the scale of the project.
+            My diverse experience in research development, the visual arts, education, and event planning has been instrumental in shaping my skill set and passion for bringing things from  <strong> ideation to implementation</strong>. The core principles that guide me and the roles I take on include <strong>creativity, a people-first mindset, and accessibility</strong>. 
+            This commitment to turning vision into reality <strong>with intentionality</strong> is what drives my interest in Product Management, particularly within (Bio)Tech Development, Strategy, and Data Analytics. I want to do work that solves real-world problems with high effort and attention to detail no matter the scale of the project.
           </p>
           <p className="ep-sub">
             This portfolio is a documentation of the work I'm most proud of, showcasing how concepts are translated into <strong>tangible products and initiatives designed to thrive</strong>.
